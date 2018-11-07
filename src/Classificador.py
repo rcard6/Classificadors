cccc1e3ce8095a14cfafd340056813743f4121b8
rcard <ricardbp6@gmail.com>
2018-11-07 17:07:17 +0100

    Calcular error de classificacio per reg. logistic i SVM(per als 3 kernels) i això 3 cops (basica, k-fold, leave-one...

--- src/Classificador.py
+++ src/Classificador.py
@@ -7,7 +7,7 @@ class Classificador(object):
 
     def load_dataset(self, path):
         data = np.genfromtxt(path, delimiter=',', skip_header=1)
-        x = data.astype('int32')
+        x = data
         # Triatge output (parkinsons = status(-7), parkinsons_updrs = motor_UPDRS(4) o total_UPDRS(5))
         y = data[:, 4].astype('int32')
         x = np.delete(x, 4, axis=1)
@@ -15,7 +15,7 @@ class Classificador(object):
 
     # < img src = "images/table_1.png" width = "80%" > # Ens recomanen fer una taula amb els valors obtinguts
 
-    def split_data(self, x, y, train_ratio=0.8):
+    def split_data(self, x, y, train_ratio):
         indices = np.arange(x.shape[0])
         np.random.shuffle(indices)
         n_train = int(np.floor(x.shape[0] * train_ratio))
@@ -28,10 +28,5 @@ class Classificador(object):
         return x_train, y_train, x_val, y_val
 
     def train_svm(self, x, y, kernel='linear', C=0.01, gamma=0.001, probability=True):
-        if kernel == 'linear':
-            svclin = SVC(C=C, kernel=kernel, gamma=gamma, probability=probability)
-        if kernel == 'poly':
-            svclin = svm.SVC(C=C, kernel=kernel, gamma=gamma, probability=probability)
-        if kernel == 'rbf':
-            svclin = svm.SVC(C=C, kernel=kernel, gamma=gamma, probability=probability)  # l'entrenem
-        return svclin.fit(x, y)
+        svclin = SVC(C=C, kernel=kernel, gamma=gamma, probability=probability)
+        return svclin.fit(x.astype(float), y.astype(int))
--- src/Main.py
+++ src/Main.py
@@ -7,19 +7,60 @@ if __name__ == '__main__':
     cl = src.Classificador.Classificador()
     x, y = cl.load_dataset('parkinsons_updrs.data')
 
+    # x = x.astype('float32')
+    # y = y.astype('float32')
+
+    print(x.shape);
+    print(y.shape)
+
+    tipus = ''
+
+    # Bàsica
+    tipus = 'Bàsica'
+    # Logistic Regression
     particions = [0.5, 0.7, 0.8]
 
-    # for part in particions:
-    #     x_t, y_t, x_v, y_v = cl.split_data(x, y, part)
-    #     # Creem el regresor logístic
-    #     logireg = LogisticRegression(C=2.0, fit_intercept=True, penalty='l2', tol=0.001)
-    #     # l'entrenem
-    #     logireg.fit(x_t, y_t)
-    #
-    #     print("Correct classification Logistic ", part, "%: ", logireg.score(x_v, y_v))
-    #     y_pred = logireg.predict(x_v)
-    #     percent_correct_log = np.mean(y_v == y_pred).astype('float32')
-    #     print("Correct classification Logistic ", part, "%: ", percent_correct_log, "\n")
+    # SVM - Tipus kernel:
+    # Linear
+    kernel = 'linear'
+    # Polinomial
+    # kernel = 'poly' - Descomentar quan estigui fet el switch
+    # Gaussia
+    # kernel = 'rbf' - Descomentar quan estigui fet el switch
 
-    print(type(x[0, 0]))
-    print(cl.train_svm(x, y, kernel='linear', C=0.01, gamma=0.001, probability='True'))
+    # k-Fold Validation
+    tipus = 'K-fold validation'
+    # Logistic Regression
+
+    # SVM - Tipus kernel:
+    # Linear
+    # Polinomial
+    # Gaussia
+
+    # Leave-One-Out-Cross-Validation
+    tipus = 'Leave-One-Out-Cross-Validation'
+    # Logistic Regression
+
+    # SVM - Tipus kernel:
+    # Linear
+    # Polinomial
+    # Gaussia
+
+
+    # ---------------------------------------------------------------------------------------------------------
+    # Codi regressor logistic
+    for part in particions:
+        x_t, y_t, x_v, y_v = cl.split_data(x, y, part)
+        # Creem el regresor logístic
+        logireg = LogisticRegression(C=2.0, fit_intercept=True, penalty='l2', tol=0.001)
+        # l'entrenem
+        logireg.fit(x_t, y_t)
+
+        print("Correct classification Logistic ", part, "%: ", logireg.score(x_v, y_v))
+        y_pred = logireg.predict(x_v)
+        percent_correct_log = np.mean(y_v == y_pred).astype('float32')
+        print("Correct classification Logistic ", part, "%: ", percent_correct_log, "\n")
+
+    # Crida a codi SVM
+    print(cl.train_svm(x, y, kernel=kernel, C=0.01, gamma=0.001, probability=True))
+
