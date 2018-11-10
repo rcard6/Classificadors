0bac7fb1e6606a7f147ec5d2206e226c19db19e9
Emilio Garcia <emilio.garciaq@e-campus.uab.cat>
2018-11-10 15:27:47 +0100

    Changing the call parent constructor order.

--- src/SVM.py
+++ src/SVM.py
@@ -5,11 +5,12 @@ from Classificador import Classificador
 class SVM(Classificador):
 
     def __init__(self, path, applicationmethod, kernel='linear', c=0.01, gamma=0.001, probability=True):
+        Classificador.__init__(self, path, applicationmethod)
         self.kernel = kernel
         self.C = c
         self.gamma = gamma
         self.probability = probability
-        Classificador.__init__(self, path, applicationmethod)
+
 
     def train(self):
         svclin = SVC(self.C, self.kernel, self.gamma, self.probability)
